a131129a0b0338edf1a5edfd58ecb76a3ac6df43
Denis1988 <denis1988@gmail.com>
2020-04-01 11:43:09 +0300

    Update Assignment_1_templace.c

--- Computer science introduction 61101/HomeWork1/Assignment_1_templace.c
+++ Computer science introduction 61101/HomeWork1/Assignment_1_templace.c
@@ -1,28 +1,36 @@
-int main()
-{
-int select=0, i, all_Ex_in_loop=0;
-printf("Run menu once or cyclically?\n(Once - enter 0, cyclically
-- enter other number) ");
-if (scanf("%d", &all_Ex_in_loop) == 1)
-do
-{
-for (i=1; i<=6; i++)
-printf("Ex%d--->%d\n", i, i);
-printf("EXIT-->0\n");
-do {
-select=0;
-printf("please select 0-6 : ");
-scanf("%d",&select);
-} while ((select<0)||(select>6));
-switch (select)
-{
-case 1: Ex1(); break;
-case 2: Ex2(); break;
-case 3: Ex3(); break;
-case 4: Ex4(); break;
-case 5: Ex5(); break;
-case 6: Ex6(); break;
-}
-} while (all_Ex_in_loop && select);
-return 0;
+#include  <stdio.h>
+void Ex1();
+void Ex2();
+void Ex3();
+void Ex4();
+void Ex5();
+void Ex6();
+
+int main(){
+    int select=0, i, all_Ex_in_loop=0;
+    printf("Run menu once or cyclically?\n(Once - enter 0, cyclically - enter other number) ");
+    
+    if (scanf("%d", &all_Ex_in_loop) == 1)
+    do{
+        for (i=1; i<=6; i++)
+        printf("Ex%d--->%d\n", i, i);
+        printf("EXIT-->0\n");
+        do {
+            select=0;
+            printf("please select 0-6 : ");
+            scanf("%d",&select);
+          }
+        
+        while ((select<0)||(select>6));
+        switch (select){
+            case 1: Ex1(); break;
+            case 2: Ex2(); break;
+            case 3: Ex3(); break;
+            case 4: Ex4(); break;
+            case 5: Ex5(); break;
+            case 6: Ex6(); break;
+        }
+    } 
+    while (all_Ex_in_loop && select);
+    return 0;
 }
