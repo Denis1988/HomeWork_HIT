deaecb9b381d8ecc632811b6625ab1892129869d
Denis1988 <denis1988@gmail.com>
2020-04-01 14:41:21 +0300

    Update Ex2.c

--- Computer science introduction 61101/HomeWork1/Ex2.c
+++ Computer science introduction 61101/HomeWork1/Ex2.c
@@ -3,7 +3,7 @@
 int main(){
     int n;
     scanf("%d",&n);
-    
+    if(n < 8){printf("Number is too small");return;}
     for(int i = 0;i < 100;i++){
         for(int j = 0;j < 100;j++){
             if(((3 * i) + (5 * j)) == n){printf("(%d,%d)",i,j);}
