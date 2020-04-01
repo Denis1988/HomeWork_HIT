bd91b9f1ad89330de25b60af6d47578391deb6ee
Denis1988 <denis1988@gmail.com>
2020-04-01 14:38:55 +0300

    Update Ex2.c

--- Computer science introduction 61101/HomeWork1/Ex2.c
+++ Computer science introduction 61101/HomeWork1/Ex2.c
@@ -1,1 +1,13 @@
-test
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    
+    for(int i = 0;i < 100;i++){
+        for(int j = 0;j < 100;j++){
+            if(((3 * i) + (5 * j)) == n){printf("(%d,%d)",i,j);}
+            if(((3*i)>= n) || ((5*j)>= n)){break;}
+        }
+    }
+}
