30a1acbe550f7b85c62c32ca611184c516174b39
Denis1988 <denis1988@gmail.com>
2020-04-05 16:23:09 +0300

    Update Ex3.c

--- Computer science introduction 61101/HomeWork1/Ex3.c
+++ Computer science introduction 61101/HomeWork1/Ex3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    int n,q,w,e,r,t,y;
+    int n,q,w,e,r,t,y,tmp;
     printf("Please enter number of numbers to be checked(for this moment 6 is maximum) : ");
     scanf("%d",&n);
     
@@ -10,24 +10,31 @@ int main(){
     for(int i = 1; i < n;i++){
         switch i{
             case 1:
-            scanf("%d",&q);
+            scanf("%d",&tmp);
+            if(ifLessThanZero(tmp))
             break;
             case 2:
-            scanf("%d",&q);
+            scanf("%d",&tmp);
             break;
             case 3:
-            scanf("%d",&q);
+            scanf("%d",&tmp);
             break;
             case 4:
-            scanf("%d",&q);
+            scanf("%d",&tmp);
             break;
             case 5:
-            scanf("%d",&q);
+            scanf("%d",&tmp);
             break;
             case 6:
-            scanf("%d",&q);
+            scanf("%d",&tmp);
             break;
         }
     }
+    int min = 
     
+}
+
+int ifLessThanZero(int t){
+    if t < 0{return 0;}
+    else{return 1;}
 }
