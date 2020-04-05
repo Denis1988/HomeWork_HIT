0d1aad337266d68322d79c5a4127e30096c0d90f
Denis1988 <denis1988@gmail.com>
2020-04-05 16:19:13 +0300

    Create Ex3.c

---
+++ Computer science introduction 61101/HomeWork1/Ex3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+    int n,q,w,e,r,t,y;
+    printf("Please enter number of numbers to be checked(for this moment 6 is maximum) : ");
+    scanf("%d",&n);
+    
+    printf("Enter numbers to be checked one by one : ");
+    
+    for(int i = 1; i < n;i++){
+        switch i{
+            case 1:
+            scanf("%d",&q);
+            break;
+            case 2:
+            scanf("%d",&q);
+            break;
+            case 3:
+            scanf("%d",&q);
+            break;
+            case 4:
+            scanf("%d",&q);
+            break;
+            case 5:
+            scanf("%d",&q);
+            break;
+            case 6:
+            scanf("%d",&q);
+            break;
+        }
+    }
+    
+}
