f9dc335e294155d8971e87f0e584f83999224e1a
Denis1988 <denis1988@gmail.com>
2020-05-10 18:31:22 +0300

    Update Ex3.c

--- Computer science introduction 61101/HomeWork2/Ex3.c
+++ Computer science introduction 61101/HomeWork2/Ex3.c
@@ -2,27 +2,24 @@
 int req(int n);
 int main()
 {
-    int number = 0;
-    printf("Enter the number : ");
-    scanf("%d",&number);
-    printf("Ans : %d",req(number));
+	int number = 0;
+	printf("Enter the number : ");
+	scanf("%d", &number);
+	printf("Ans : %d", req(number));
 
-    return 0;
+	return 0;
 }
-int req(int n){
-    int static ans;
-    int static count = 1;
-    if(n > 0){
-        count *= 10;
-        if(n%10 < 9){
-            ans += ((n%10)+1)*count;
-        }
-        else{
-            ans *= count;
-        }
-        req(n/10);
-    }
-    if(n == 0){
-        return ans/10;
-    }
+int req(int n) {
+	int static ans;
+	int static count = 1;
+	if (n > 0) {
+		count *= 10;
+		if (n % 10 < 9) {
+			ans += ((n % 10) + 1)*count;
+		}
+		req(n / 10);
+	}
+	if (n == 0) {
+		return ans / 10;
+	}
 }
