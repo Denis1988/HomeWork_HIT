260d05ec66e66c6ee1d1a0c71694aaa15390ac82
Denis1988 <denis1988@gmail.com>
2020-08-23 11:37:54 +0300

    Update Ex3.c

--- Sadna/HW1/Ex3.c
+++ Sadna/HW1/Ex3.c
@@ -1,60 +1,65 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
-int* resizeArray(int* array,int n,int m);
-int* multiplyMatrix(int *a,int *b,int m,int n,int p);
+#include <stdlib.h>
+void multiplyMatrix(int *a,int *b,int m,int n,int p,int* ans);
+void extener(int* a,int n,int m);
 
 int main()
 {
-    int x[10][10] = {0};
-    int a[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-    int b[3][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
-    
-    int* ptra = &a[0][0];
-    int* ptrb = &b[0][0];
-    
-    multiplyMatrix(ptra,ptrb,3,3,4);
+int a[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+int b[3][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+
+int* ptra = &a;
+int* ptrb = &b;
+int* ans = (int*)malloc(2*2*sizeof(int));
+multiplyMatrix(ptra,ptrb,3,3,4,ans);
+
+printf("after extend\n");
+for(int i = 0;i < 3 ;i++){
+  for(int j = 0;j < 4;j++){
+     printf("%d ",ans[i*2+j]);
+  }
+  printf("\n");
+}
+
     return 0;
 }
 
-int* multiplyMatrix(int *a,int *b,int m,int n,int p){
-    int ans[2][2] = {0};
-    
-    int mn = (m > n) ? m : n;
-    int mm = (n > p) ? n : p;
-    
-    int *ansptr = &ans[0][0];
-    
-    for(int i = 0;i < mn;i++){
-        for(int j = 0;j < mm;j++){
-            if(i > 2 || j > 2){
-                ansptr = resizeArray(ansptr,i,j);
-            }
-            ans[i][j] = (a[i*m+j]) * (b[i*n+j]);
-            printf("%d ",ans[i][j]);
-        }
-        printf("\n");
-    }
-    return ansptr;
+void extener(int* a,int n,int m){
+
+int *nx = (int*)calloc((n+1)*(m+1),sizeof(int));
+
+for(int i = 0;i < n ;i++){
+  for(int j = 0;j < m;j++){
+      nx[i*2+j] = a[i*2+j];
+  }
 }
 
-int* resizeArray(int* array,int n,int m){
-    int ret[n][m] = {0};
-    
-    for(int i = 0;i < n;i++){
-        for(int j = 0;j < m;j++){
-            ret[i][j] = array[i*n + j];
-            printf("%d ",ret[i][j]);
+a = nx;
+free(nx);
+
+}
+
+void multiplyMatrix(int* a,int* b,int m,int n,int p,int* ans){
+int mn = (m > n) ? m : n;
+int mm = (n > p) ? n : p;
+
+    for(int i = 0;i < mn;i++){
+        for(int j = 0;j < mm;j++){
+            if(i >= m-1 || j >= p-1){
+                extener(ans,i+1,j+1);
+            }
+            if(i >= m-1){
+                ans[i*mn + j] = 0*b[i*mn+j];
+                break;
+            }
+            if(j >= p-1){
+                ans[i*mn + j] = a[i*mn+j]*b[i*mn+j];
+                break;
+            }
+            ans[i*mn + j] = a[i*mn+j]*b[i*mn+j];
+            printf("%d ",ans[i*mn + j]);
         }
         printf("\n");
     }
     
-    int* ptr = &ret[0][0];
-    return ptr;
 }
